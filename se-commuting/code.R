a2b1cce8c8d0c028e70b2d6c84d2b06019f2bbcf
Henrik Lindberg <henrik@zimpler.com>
2017-07-30 14:12:39 +0200

    Complete se-commuting

--- se-commuting/code.R
+++ se-commuting/code.R
@@ -10,3 +10,33 @@ df <- read_tsv('pendling.scb', locale = locale(encoding = 'iso8859-1')) %>%
             year = år,
             n = `Förvärvsarbetande pendlare 16+ år`) %>%
   separate(region, into=c('code', 'name'), sep=' ', extra = 'merge')
+
+
+
+df %>%
+  filter(year == max(year)) %>%
+  group_by(name) %>%
+  spread(variable, n) %>%
+  mutate(p = (coming_in - going_out) / (going_out + staying),
+         `in`=coming_in + staying,
+         out=going_out + staying) %>%
+  arrange(p) %>%
+  ggplot(aes(going_out + staying, p, size=coming_in + staying)) +
+    geom_point(color='black', fill='#D3BC5F', alpha=0.5, shape=21) +
+    scale_size_area(name="Arbetande", max_size = 20, breaks=c(10, 100, 500) * 1e3) +
+    scale_y_continuous(labels=scales::percent, breaks = -2:4/4) +
+    scale_x_log10(limits=c(NA, 700e3)) +
+    annotation_logticks(sides = 'b') +
+    labs(x="", y="", caption="@hnrklndbrg | Källa: SCB") +
+    theme_henrik(grid='XY', legend.position='bottom')
+
+ggsave('/tmp/out.svg', width=8, height=7)
+
+
+
+
+
+
+
+
+
